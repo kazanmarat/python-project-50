65d7d2290ae82cde86574a9d57701e0944be0543
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2024-01-17 08:20:26 +0300

    wrap code in main function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,13 +2,17 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(
-    description='Compares two configuration files and shows a difference.',
-    prog='gendiff')
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.',
+        prog='gendiff')
 
-parser.add_argument('first_file')
+    parser.add_argument('first_file')
 
-parser.add_argument('second_file')
+    parser.add_argument('second_file')
 
-args = parser.parse_args()
-print(args)
+    args = parser.parse_args()
+    print(args)
+
+if __name__ == "__main__":
+    main()
