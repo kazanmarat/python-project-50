d33b5817efe7c71ae0d2f7d90103ad13da203e38
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2024-01-31 08:22:10 +0300

    move logic from gendiff.py to function collect_arguments in cli.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
 
-import argparse
+import cli
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.',
-        prog='gendiff')
-
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f','--format', help='set format of output')
-
-    args = parser.parse_args()
-    print(args)
+    cli.collect_arguments()
 
 if __name__ == "__main__":
     main()
