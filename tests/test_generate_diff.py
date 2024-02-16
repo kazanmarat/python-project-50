9826a4eabbec0afe35582a1d723239157a4d79ba
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2024-02-16 08:45:16 +0300

    change test for new files name and new view

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,10 +1,18 @@
-import json
+import pytest
 from gendiff import generate_diff
 
+flat_file = open('tests/fixtures/test_flat.txt', 'r')
+flat_output = flat_file.read()
+flat_file.close()
+flat_json = 'tests/fixtures/filepath1.json', 'tests/fixtures/filepath2.json'
+# flat_yml = 'tests/fixtures/filepath1.yml', 'tests/fixtures/filepath2.yml'
 
-def test_generate_diff():
-    path = 'tests/fixtures/file1.json', 'tests/fixtures/file2.json'
-    result_file = open('tests/fixtures/test_flat.txt', 'r')
-    result = result_file.read()
-    result_file.close()
+flat_cases = [
+    (flat_json, flat_output)
+    # (flat_yml, flat_output)
+]
+
+
+@pytest.mark.parametrize('path, result', flat_cases)
+def test_generate_diff(path, result):
     assert generate_diff(path) == result
