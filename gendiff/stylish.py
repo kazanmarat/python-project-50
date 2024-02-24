e3d9351bfb274d9c35fda620e04a4caf5b7681e6
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2024-02-24 14:21:42 +0300

    reduce complexity

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,8 +1,10 @@
 import itertools
 
 
+INDENT = ' '
+
+
 def add_prefix(content, depth, symb=''):
-    INDENT = ' '
     depth_indent = INDENT * (depth * 4 - len(symb))
     return f"{depth_indent}{symb}{content}"
 
@@ -15,45 +17,38 @@ def rename_bool(word):
         return word
 
 
-def format(dif):
-    def inner(dif, depth):
-        if not isinstance(dif, dict):
-            return f'{rename_bool(dif)}'
-        current_indent = INDENT * 4 * depth
-        current_depth = depth + 1
-        lines = []
-        for key, descrip in dif.items():
-            # when descrip just value
-            if not isinstance(descrip, dict):
-                lines.append(f'{add_prefix(key, current_depth)}: '
-                             + f'{inner(descrip, current_depth)}')
-                continue
-            value = descrip.get('value')
-            type = descrip.get('type')
-            if type == 'added':
-                lines.append(f'{add_prefix(key, current_depth, symb="+ ")}: '
-                             + f'{inner(value, current_depth)}')
-            elif type == 'deleted':
-                lines.append(f'{add_prefix(key, current_depth, symb="- ")}: '
-                             + f'{inner(value, current_depth)}')
-            elif type == 'unchanged':
-                lines.append(f'{add_prefix(key, current_depth, symb="  ")}: '
-                             + f'{inner(value, current_depth)}')
-            elif type == 'changed':
-                lines.append(f'{add_prefix(key, current_depth, symb="- ")}: '
-                             + f'{inner(value[0], current_depth)}')
-                lines.append(f'{add_prefix(key, current_depth, symb="+ ")}: '
-                             + f'{inner(value[1], current_depth)}')
-            elif type == 'nested':
-                lines.append(f'{add_prefix(key, current_depth, symb="  ")}: '
-                             + f'{inner(value, current_depth)}')
-            # if nested dict without changes
-            else:
-                lines.append(f'{add_prefix(key, current_depth)}: '
-                             + f'{inner(descrip, current_depth)}')
+def format(dif, depth=0):
+    if not isinstance(dif, dict):
+        return f'{rename_bool(dif)}'
+    current_indent = INDENT * 4 * depth
+    current_depth = depth + 1
+    types = {'added': "+ ",
+             'deleted': "- ",
+             'unchanged': "  ",
+             'nested': "  "
+             }
+    lines = []
+    for key, descrip in dif.items():
+        # when descrip just value
+        if not isinstance(descrip, dict):
+            lines.append(f'{add_prefix(key, current_depth)}: '
+                         + f'{format(descrip, current_depth)}')
+            continue
+        value = descrip.get('value')
+        type = descrip.get('type')
+        if type in types:
+            prefix = types[type]
+            lines.append(f'{add_prefix(key, current_depth, prefix)}: '
+                         + f'{format(value, current_depth)}')
+        elif type == 'changed':
+            lines.append(f'{add_prefix(key, current_depth, symb="- ")}: '
+                         + f'{format(value[0], current_depth)}')
+            lines.append(f'{add_prefix(key, current_depth, symb="+ ")}: '
+                         + f'{format(value[1], current_depth)}')
+        # if nested dict without changes
+        else:
+            lines.append(f'{add_prefix(key, current_depth)}: '
+                         + f'{format(descrip, current_depth)}')
 
-        result = itertools.chain('{', lines, [current_indent + '}'])
-        return '\n'.join(result)
-    depth = 0
-    INDENT = ' '
-    return inner(dif, depth)
+    result = itertools.chain('{', lines, [current_indent + '}'])
+    return '\n'.join(result)
